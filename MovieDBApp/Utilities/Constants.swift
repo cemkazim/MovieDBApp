5b88ca63b58f9a0d6cd1342db56c428ce7b3b707
Cem Kazım <cemkazim@hotmail.com>
2021-07-01 22:42:07 +0300

    first commit

---
+++ MovieDBApp/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MovieDBApp
+//
+//  Created by Cem Kazım on 1.07.2021.
+//
+
+import Foundation
+
+public struct Constants {
+    static let movieListTitle = "Movie List"
+}
